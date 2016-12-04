da3315315dcca30d1ffc40477ec63aeba06ffc8f
Laurent Shala <laurentshala@icloud.com>
2016-12-04 11:35:52 -0500

    fixed the sign up page

--- Hackathon2016/SignUpViewController.swift
+++ Hackathon2016/SignUpViewController.swift
@@ -8,26 +8,50 @@
 
 import UIKit
 import Firebase
+import NVActivityIndicatorView
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    var spinner: NVActivityIndicatorView?
+    var loadingView: UIView = UIView()
+
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordTwoTextField: UITextField!
     
     
-    
-    
     @IBAction func signInButton(_ sender: UIButton) {
+        showActivityIndicator()
+
         
         if(passwordTextField.text != passwordTwoTextField.text) {
-            print("Passwords are not the same!")
+            let alertController = UIAlertController(title: "Sign Up Error", message: "The passwords are not the same", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            passwordTextField.text = nil
+            passwordTwoTextField.text = nil
             return
         }
         
-        FIRAuth.auth()!.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { user, error in
-            if error == nil {
+        FIRAuth.auth()!.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (FIRUser, Error) in
+            if Error == nil {
                 FIRAuth.auth()!.signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!)
+                self.hideActivityIndicator()
+                let alertController = UIAlertController(title: "Signed Up", message: "You are now signed up", preferredStyle: UIAlertControllerStyle.alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+
+            }else {
+                print(Error!)
+                self.hideActivityIndicator()
+                let alertController = UIAlertController(title: "Sign up Error", message: "There was sign up problem", preferredStyle: UIAlertControllerStyle.alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+                sender.isEnabled = true
             }
         }
     }
@@ -49,6 +73,29 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return false
+    }
+
+    
+    func showActivityIndicator() {
+        self.loadingView = UIView()
+        self.loadingView.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        self.loadingView.center = self.view.center
+        self.loadingView.alpha = 0.9
+        self.loadingView.clipsToBounds = true
+        self.loadingView.layer.cornerRadius = 10
+        
+        self.spinner = NVActivityIndicatorView(frame: self.view.frame, type: NVActivityIndicatorType.ballClipRotatePulse, color: UIColor.white, padding: nil)
+        self.spinner?.frame = CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0)
+        self.spinner?.center = CGPoint(x:self.loadingView.bounds.size.width / 2, y:self.loadingView.bounds.size.height / 2)
+        
+        self.loadingView.addSubview(self.spinner!)
+        self.view.addSubview(self.loadingView)
+        self.spinner?.startAnimating()
+    }
+    
+    func hideActivityIndicator() {
+        self.spinner?.stopAnimating()
+        self.loadingView.removeFromSuperview()
     }
 
     
